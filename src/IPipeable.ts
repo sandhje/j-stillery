05c2b4097fbe2e0bc1bb36eebefe3850d46f74f5
CONCEPTS\sbouw <sandhje.bouw@telfortglasvezel.nl>
2017-03-14 07:44:57 +0100

    Update Pipeable and Pipeline interfaces

--- src/IPipeable.ts
+++ src/IPipeable.ts
@@ -2,21 +2,21 @@ import { IStage } from ".";
 
 /**
  * Generic pipeable interface
- * 
+ *
  * Defines a pipe method which allows for configuring the subject with stages
- * 
+ *
  * @author Sandhjé
  */
 interface IPipeable<T>
 {
     /**
      * Pipe a stage onto the subject
-     * 
+     *
      * Configure the subject with stages, usually so the can be run
      * Return self to achieve a more fluent API
-     * 
+     *
      * @param IStage<T> stage
-     * @returns IPipeline<T>
+     * @returns IPipeable<T>
      */
     pipe: (stage: IStage<T>) => IPipeable<T>;
 }
--- src/IPipeline.ts
+++ src/IPipeline.ts
@@ -1,14 +1,34 @@
-import { IPipeable, IRunnable, IStage } from ".";
+import { IStage } from ".";
 
 /**
  * Generic pipeline interface
- * 
+ *
  * Implementors of the pipeline interface need to be both pipeable and runnable
- * 
- * @see ./IRunnable
- * @see ./IPipeable
+ *
  * @author Sandhjé
  */
-interface IPipeline<T> extends IRunnable<T>, IPipeable<T> { }
+interface IPipeline<T> {
+    /**
+     * Pipe a stage onto the subject
+     *
+     * Configure the subject with stages, usually so the can be run
+     * Return self to achieve a more fluent API
+     *
+     * @param IStage<T> stage
+     * @returns IPipeline<T>
+     */
+    pipe: (stage: IStage<T>) => IPipeline<T>;
+
+    /**
+     * Run the subject
+     *
+     * Return a Promise which resolves with the output resulting
+     * from the subject being run with a given input
+     *
+     * @param T input
+     * @returns Promise<T>
+     */
+    run: (input: T) => Promise<T>;
+}
 
 export { IPipeline };
